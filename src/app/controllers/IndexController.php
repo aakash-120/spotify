c5858ad13d8681efd8e2ee19c6b912ee191b3786
aakash_vishwakarma <aakashvishwakarma@cedcoss.com>
2022-04-11 13:17:29 +0530

    raw track

--- src/app/controllers/IndexController.php
+++ src/app/controllers/IndexController.php
@@ -33,7 +33,7 @@ class IndexController extends Controller
     //     $ch = curl_init();
     //     curl_setopt($ch, CURLOPT_URL,            'https://accounts.spotify.com/api/token' );
     //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
-    //     curl_setopt($ch, CURLOPT_POST,           1 );
+    //     curl_setopt($ch, CURLOPT_POST,          312wujhiq6rtj3m4kqzvvhfxyjxi 1 );
     //     curl_setopt($ch, CURLOPT_POSTFIELDS,     'grant_type=client_credentials' ); 
     //     curl_setopt($ch, CURLOPT_HTTPHEADER,     array('Authorization: Basic '.base64_encode($client_id.':'.$client_secret))); 
 
@@ -58,6 +58,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $result = json_decode(curl_exec($ch), true);
 curl_close($ch);
 print_r($result);
+$this->view->result = $result;
 
 
 
@@ -83,7 +84,7 @@ print_r($result);
         // var_dump($token->getToken());
         // var_dump($token->getExpires());
         // echo '</pre>';
-        die;
+      //  die;
     }
 
 
@@ -95,4 +96,137 @@ print_r($result);
         //ugc-image-upload user-modify-playback-state user-read-playback-state user-read-currently-playing user-follow-modify user-follow-read user-read-recently-played user-read-playback-position user-top-read playlist-read-collaborative playlist-modify-public playlist-read-private playlist-modify-private app-remote-control streaming user-read-email user-read-private user-library-modify user-library-read
         die;
     }
+
+
+    public function searchAction()
+    {
+        print_r($_POST);
+
+        $client_id = '829c80e105f54dd48f1af2758c73619b'; 
+        $client_secret = 'ae4dd45d49304d1692d89a45a5f75dca';
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,            'https://accounts.spotify.com/api/token' );
+        curl_setopt($ch, CURLOPT_HTTPHEADER,     array('Authorization: Basic '.base64_encode($client_id.':'.$client_secret))); 
+        curl_setopt($ch, CURLOPT_POSTFIELDS,     'grant_type=client_credentials' ); 
+        curl_setopt($ch, CURLOPT_POST,           1 );
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:x.x.x) Gecko/20041107 Firefox/x.x");
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        
+        $json = curl_exec($ch);
+        $json = json_decode($json);
+        curl_close($ch);
+        
+        echo '<pre>'.print_r($json, true).'</pre>';
+        echo $json->access_token;
+        echo "-------------------------------------------------------";
+
+
+
+        if(isset($_POST['search']))
+        {
+            $search = $_POST['search'];
+        }
+        
+        if(isset($_POST['album']))
+        {
+            $selected_field = $_POST['album'];
+            $authorization = "Authorization: Bearer " . $json->access_token;
+            $spotifyURL = 'https://api.spotify.com/v1/search?q='.urlencode($search).'&type=album';
+
+            $ch2 = curl_init();
+            curl_setopt($ch2, CURLOPT_URL, $spotifyURL);
+            curl_setopt($ch2,  CURLOPT_HTTPHEADER, array('Content-Type: application/json' , $authorization));
+            curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch2, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:x.x.x) Gecko/20041107 Firefox/x.x");
+            curl_setopt($ch2, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($ch2, CURLOPT_SSL_VERIFYPEER, false);
+            $json2 = curl_exec($ch2);
+            $json2 = json_decode($json2);
+            curl_close($ch2);
+
+            echo '<pre>'.print_r($json2, true).'</pre>';
+        }
+
+        if(isset($_POST['artist']))
+        {
+            $selected_field = $_POST['artist'];
+            $authorization = "Authorization: Bearer " . $json->access_token;
+ 
+            
+
+            $spotifyURL = 'https://api.spotify.com/v1/search?q='.urlencode($search).'&type=artist';
+
+            $ch2 = curl_init();
+
+
+            curl_setopt($ch2, CURLOPT_URL, $spotifyURL);
+            curl_setopt($ch2,  CURLOPT_HTTPHEADER, array('Content-Type: application/json' , $authorization));
+            curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch2, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:x.x.x) Gecko/20041107 Firefox/x.x");
+            curl_setopt($ch2, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($ch2, CURLOPT_SSL_VERIFYPEER, false);
+            $json2 = curl_exec($ch2);
+            $json2 = json_decode($json2);
+            curl_close($ch2);
+
+            echo '<pre>'.print_r($json2, true).'</pre>';
+        }
+
+        // if(isset($_POST['playlist']))
+        // {
+        //     $selected_field = $_POST['playlist'];
+        // }
+
+        if(isset($_POST['track']))
+        {
+            $selected_field = $_POST['track'];
+            $authorization = "Authorization: Bearer " . $json->access_token;        
+            $spotifyURL = 'https://api.spotify.com/v1/search?q='.urlencode($search).'&type=track';
+
+            $ch2 = curl_init();
+            curl_setopt($ch2, CURLOPT_URL, $spotifyURL);
+            curl_setopt($ch2,  CURLOPT_HTTPHEADER, array('Content-Type: application/json' , $authorization));
+            curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch2, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:x.x.x) Gecko/20041107 Firefox/x.x");
+            curl_setopt($ch2, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($ch2, CURLOPT_SSL_VERIFYPEER, false);
+            $json2 = curl_exec($ch2);
+            $json2 = json_decode($json2);
+            curl_close($ch2);
+
+            echo '<pre>'.print_r($json2, true).'</pre>';
+            $this->view->track = $json2;
+        }
+
+        // if(isset($_POST['show']))
+        // {
+        //     $selected_field = $_POST['show'];
+        // }
+
+        // if(isset($_POST['episode']))
+        // {
+        //     $selected_field = $_POST['episode'];312wujhiq6rtj3m4kqzvvhfxyjxi
+        // }
+
+        // echo $search;
+        // echo $album;
+        // echo $artist;
+        // echo $playlist;
+        // echo $track;
+        // echo $show;
+        // echo $episode;
+
+
+
+
+
+
+
+       
+       // die;
+    }
+
+
 }
